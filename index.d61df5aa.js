374b8bbc81743e39b7959c97d27e1e0e614872b5
PaulinaCichowska <114925199+PaulinaCichowska@users.noreply.github.com>
2023-07-23 15:55:50 +0000

    Deploying to gh-pages from @ GrzegorzKobuszewski/Filmoteka-Team-Project@f6b0478059f49800457d1c1f9429c7914eb4c4f8 🚀

---
+++ index.d61df5aa.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".movie-card"),n=document.querySelector(".modal"),t=document.querySelector("#close-btn");e.addEventListener("click",function(){n.style.display="block"}),t.addEventListener("click",function(){n.style.display="none"}),window.onclick=function(e){e.target==n&&(n.style.display="none")};
+//# sourceMappingURL=index.d61df5aa.js.map
