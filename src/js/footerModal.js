06c3527f6f85a657fbca5e1a377ac3776aabe828
ewasawczynska <sawczynska.e@gmail.com>
2023-07-23 18:32:49 +0200

    footer modal schema

---
+++ src/js/footerModal.js
@@ -0,0 +1,18 @@
+const footerButton = document.querySelector('.footer__button');
+const footerModal = document.querySelector('.footer__modal');
+const footerCloseBtn = document.querySelector('#footer__close-btn');
+
+function openFooterModal() {
+  footerModal.style.display = 'block';
+}
+function closeFooterModal() {
+  footerModal.style.display = 'none';
+}
+footerButton.addEventListener('click', openFooterModal);
+footerCloseBtn.addEventListener('click', closeFooterModal);
+
+window.onclick = function (e) {
+  if (e.target == footerModal) {
+    footerModal.style.display = 'none';
+  }
+};
