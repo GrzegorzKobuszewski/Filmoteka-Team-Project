9f74066c3eb544a5a600c8fa28f8a8918745e694
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-07-30 02:38:10 +0200

    modal Kasia i Bartek

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,26 +1,42 @@
 const movieCards = document.querySelectorAll('.movie-card');
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.backdrop');
 const closeBtn = document.querySelector('#close-btn');
+import { html } from './pagination.js';
 
-function openModal() {
-  modal.style.display = 'block';
+export function openModal() {
+  //podstawiam zmienną html pod zawartość znacznika <div id='detalisMovie'>
+  const detailsElement = document.getElementById('detailsMovie');
+  if (detailsElement) {
+    detailsElement.innerHTML = html;
+  }
+
+  modal.style.display = 'flex';
 }
 
 function closeModal() {
   modal.style.display = 'none';
 }
 
-movieCards.forEach(movieCard => {
-  movieCard.addEventListener('click', openModal);
-});
+// movieCards.forEach(movieCard => {
+//   movieCard.addEventListener('click', openModal);
+// });
 
 closeBtn.addEventListener('click', closeModal);
+// modal.addEventListener('click', closeModal);
 
-window.onclick = function (e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-};
+// modal.addEventListener('click', event => {
+//   // Sprawdzamy, czy kliknięcie było na modalu
+//   if (event.target.classList.contains('modal')) {
+//   } else {
+//     closeModal();
+//   }
+// });
+
+// window.onclick = function (e) {
+//   if (e.target === modal) {
+//     modal.style.display = 'none';
+//   }
+// };
 
 window.onkeydown = function (e) {
   if (e.key === 'Escape') {
